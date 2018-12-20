7a053d9d4ebd2c59bac25e487e030a0c629f2b70
Zoltan Farkas <zolyfarkas@yahoo.com>
2018-12-20 15:56:41 -0500

    [add] failsafe invocations.

--- src/main/java/org/spf4j/jaxrs/client/FailsafeInvocations.java
+++ src/main/java/org/spf4j/jaxrs/client/FailsafeInvocations.java
@@ -1,7 +1,11 @@
 package org.spf4j.jaxrs.client;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
 import java.util.concurrent.Callable;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.client.Invocation;
@@ -11,7 +15,7 @@ import javax.ws.rs.core.Response;
 import org.spf4j.base.UncheckedTimeoutException;
 import org.spf4j.failsafe.AsyncRetryExecutor;
 import org.spf4j.failsafe.HedgePolicy;
-import org.spf4j.failsafe.PartialTypedExceptionRetryPredicate;
+import org.spf4j.failsafe.RetryDecision;
 import org.spf4j.failsafe.RetryPolicy;
 import org.spf4j.failsafe.concurrent.DefaultContextAwareRetryExecutor;
 
@@ -20,22 +24,48 @@ import org.spf4j.failsafe.concurrent.DefaultContextAwareRetryExecutor;
  */
 public final class FailsafeInvocations {
 
-
-  private static final AsyncRetryExecutor<Object, Callable<? extends  Object>> DEFAULT_HTTP_RETRY_EXEC
+  private static final AsyncRetryExecutor<Object, Callable<? extends Object>> DEFAULT_HTTP_RETRY_EXEC
           = RetryPolicy.newBuilder()
-              .withDefaultThrowableRetryPredicate()
-              .withExceptionPartialPredicate(WebApplicationException.class,
-                      (WebApplicationException ex, c)  -> {
-                          int status = ex.getResponse().getStatus();
-                          switch (status) {
-                            case 408:
-                            case 409:
-                              return
-                          }
-                          return null;
-                      })
-              .withRetryOnException(Exception.class, 2) // will retry any other exception twice.
-              .build()
+                  .withDefaultThrowableRetryPredicate()
+                  .withExceptionPartialPredicate(WebApplicationException.class,
+                          (WebApplicationException ex, Callable<? extends Object> c) -> {
+                            Response response = ex.getResponse();
+                            int status = response.getStatus();
+                            switch (status) {
+                              case 408:
+                              case 409:
+                              case 419:
+                              case 420:
+                              case 423:
+                              case 429:
+                              case 440:
+                              case 449:
+                              case 503:
+                              case 504:
+                              case 509:
+                              case 522:
+                              case 524:
+                              case 599:
+                                String retryAfter = response.getHeaderString("Retry-After");
+                                if (retryAfter != null && !retryAfter.isEmpty()) {
+                                  if (Character.isDigit(retryAfter.charAt(0))) {
+                                    RetryDecision.retry(Long.parseLong(retryAfter), TimeUnit.SECONDS, c);
+                                  } else {
+                                    RetryDecision.retry(Duration.between(Instant.now(),
+                                            DateTimeFormatter.RFC_1123_DATE_TIME.parse(retryAfter, Instant::from)).toNanos(),
+                                            TimeUnit.NANOSECONDS, c);
+                                  }
+                                }
+                                return RetryDecision.retryDefault(c);
+                              default:
+                                if (status >= 400 && status < 500) {
+                                  return RetryDecision.abort();
+                                }
+                            }
+                            return null;
+                          })
+                  .withRetryOnException(Exception.class, 2) // will retry any other exception twice.
+                  .build()
                   .async(HedgePolicy.DEFAULT, DefaultContextAwareRetryExecutor.instance());
 
   public static Invocation decorate(final Invocation invocation) {
@@ -89,17 +119,17 @@ public final class FailsafeInvocations {
 
       @Override
       public Future<Response> submit() {
-          return policy.submit(invocation::invoke);
+        return policy.submit(invocation::invoke);
       }
 
       @Override
       public <T> Future<T> submit(Class<T> responseType) {
-        return  policy.submit(() -> invocation.invoke(responseType));
+        return policy.submit(() -> invocation.invoke(responseType));
       }
 
       @Override
       public <T> Future<T> submit(GenericType<T> responseType) {
-        return  policy.submit(() -> invocation.invoke(responseType));
+        return policy.submit(() -> invocation.invoke(responseType));
       }
 
       @Override
