3f218b47589832d4ffcf0aa9aaf0f90f0aafa00a
Zoltan Farkas <zolyfarkas@yahoo.com>
2020-01-31 16:25:45 -0500

    [fix] one more unit test

--- src/test/java/org/spf4j/demo/OptionsTest.java
+++ src/test/java/org/spf4j/demo/OptionsTest.java
@@ -1,8 +1,9 @@
 
 package org.spf4j.demo;
 
-import javax.ws.rs.core.MediaType;
+import com.google.common.net.HttpHeaders;
 import javax.ws.rs.core.Response;
+import org.junit.Assert;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,9 +18,11 @@ public class OptionsTest extends ServiceIntegrationBase {
 
   @Test
   public void testGetDemoRecordInfo() {
-    Response options = getTarget().request(MediaType.APPLICATION_XML_TYPE)
+    Response options = getTarget().request("application/vnd.sun.wadl+xml")
+            .header(HttpHeaders.HOST, "www.google.com")
             .options();
     LOG.debug("Received", options.getAllowedMethods());
+    Assert.assertTrue(options.getStatusInfo().getFamily() != Response.Status.Family.SERVER_ERROR);
   }
 
 
