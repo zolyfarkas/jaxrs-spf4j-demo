393c2b54301d3412bb82d693a7f3f38bd2b7099d
Zoltan Farkas <zolyfarkas@yahoo.com>
2021-09-06 09:04:32 -0400

    [fix] ignore test

--- src/test/java/org/spf4j/demo/RunLocalTest.java
+++ src/test/java/org/spf4j/demo/RunLocalTest.java
@@ -6,9 +6,11 @@ import java.net.URISyntaxException;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
 import org.junit.Assume;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.spf4j.test.log.TestUtils;
 
+@Ignore
 public class RunLocalTest {
 
   @Test(timeout = 1000000)
